e169a07514ea4f5f7d509a10108b823ced43c570
camosss <camosss@naver.com>
2025-03-17 22:50:57 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "TabPager",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "TabPager", targets: ["TabPager"]),
+    ],
+    targets: [
+        .target(name: "TabPager"),
+        .testTarget(name: "TabPagerTests", dependencies: ["TabPager"]),
+    ]
+)
---
+++ Sources/TabPager/TabPager.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct TapPager: View {
+    public var body: some View {
+        Text("Hello, TapPager!")
+    }
+}
