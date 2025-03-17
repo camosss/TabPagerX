12c3a192c7702835bcb82d1faa2e26049abe0284
camosss <camosss@naver.com>
2025-03-17 23:23:32 +0900

    Add TabPager component

--- Sources/TabPager/TabPager.swift
+++ Sources/TabPager/TabPager.swift
@@ -3,8 +3,31 @@
 
 import SwiftUI
 
-public struct TabPager: View {
+public struct TabPager<Content: View>: View {
+
+    /// List of tab titles
+    @Binding private var tabs: [String]
+
+    /// Index of the selected tab
+    @Binding private var selectedIndex: Int
+
+    /// Content view for each tab
+    private let content: (Int) -> Content
+
+    /// Designed for the user to provide the tab list, selected index, and content
+    public init(
+        tabs: Binding<[String]>,
+        selectedIndex: Binding<Int>,
+        @ViewBuilder content: @escaping (Int) -> Content
+    ) {
+        self._tabs = tabs
+        self._selectedIndex = selectedIndex
+        self.content = content
+    }
+
     public var body: some View {
-        Text("Hello, TabPager!")
+        VStack(spacing: 0) {
+
+        }
     }
 }
